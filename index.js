61fdacb4050171d42a8648f6950b3632545ea550
emilypark2004 <85959638+emilypark2004@users.noreply.github.com>
2024-02-20 00:05:53 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+async function fetchData(){
+    try{
+
+        const fruitName = document.getElementById("name").value.toLowerCase();
+        const response = await fetch(`https://www.fruityvice.com/api/fruit/${name}`);
+
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+      
+        const family = data.family;
+        const order = data.order;
+        const genus = data.genus;
+
+        const resultContainer = document.getElementById("result");
+        resultContainer.innerHTML = `
+            <h2>Fruit Information</h2>
+            <p><strong>Name:</strong> ${fruitName}</p>
+            <p><strong>Family:</strong> ${family}</p>
+            <p><strong>Order:</strong> ${order}</p>
+            <p><strong>Genus:</strong> ${genus}</p>
+        `;
+    }
+    catch(error){
+        console.error(error);
+    }
+}
